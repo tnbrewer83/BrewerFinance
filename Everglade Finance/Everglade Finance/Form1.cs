fb853fe41d8d2e57be80feb5922adafbef75d117
Mark Hill <tnbrewer83@gmail.com>
2014-09-25 19:59:55 -0400

    Added File Menu
    Added simple File Menu with only one option - Exit.

--- Everglade Finance/Everglade Finance/Form1.cs
+++ Everglade Finance/Everglade Finance/Form1.cs
@@ -16,5 +16,10 @@ namespace Everglade_Finance
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
